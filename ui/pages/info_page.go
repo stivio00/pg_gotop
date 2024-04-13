4efe5205641d3f50066d8e824f752faa8009327a
Stephen Krol <krol.stephen@gmail.com>
2024-04-13 18:17:34 +0200

    Refactor info page

--- ui/pages/info_page.go
+++ ui/pages/info_page.go
@@ -8,27 +8,47 @@ import (
 
 func CreateInfoTree(db *db.DbConnection) *tview.TreeView {
 	tree := tview.NewTreeView()
+	tree.SetGraphics(true)
 	tree.SetBackgroundColor(tcell.ColorBlue)
-	tree.SetRoot(tview.NewTreeNode("Info"))
+
+	rootNode := tview.NewTreeNode("Info")
+	rootNode.SetSelectable(true)
+	tree.SetRoot(rootNode)
 
 	settings := db.GetDbSettings()
 	connection := db.GetDbCurrentConnection()
 	locations := db.GetFileLocations()
 
-	dbconnectionMode := tview.NewTreeNode("DatabaseConnection")
-	tree.GetRoot().AddChild(dbconnectionMode)
-	dbconnectionMode.AddChild(tview.NewTreeNode("Host: " + "localhost"))
-	dbconnectionMode.AddChild(tview.NewTreeNode("Port: " + "5432"))
-	dbconnectionMode.AddChild(tview.NewTreeNode("User: " + connection.User))
-	dbconnectionMode.AddChild(tview.NewTreeNode("Database: " + connection.Database))
+	dbconnectionNode := createDbConnectionNode(connection)
+	tree.GetRoot().AddChild(dbconnectionNode)
 
-	dbNode := tview.NewTreeNode("Database")
+	dbNode := createDbNode(connection)
 	tree.GetRoot().AddChild(dbNode)
-	dbNode.AddChild(tview.NewTreeNode("version: " + connection.Version))
-	dbNode.AddChild(tview.NewTreeNode("Size: " + connection.Size))
 
-	sNode := tview.NewTreeNode("Settings")
+	sNode := createSettingsNode(settings)
 	tree.GetRoot().AddChild(sNode)
+
+	fNode := createfileNode(locations)
+	tree.GetRoot().AddChild(fNode)
+
+	return tree
+}
+
+func createfileNode(locations db.FileLocations) *tview.TreeNode {
+	fNode := tview.NewTreeNode("File Locations")
+
+	fNode.AddChild(tview.NewTreeNode("Data_directory: " + locations.Data_directory))
+	fNode.AddChild(tview.NewTreeNode("Config_file: " + locations.Config_file))
+	fNode.AddChild(tview.NewTreeNode("Hba_file: " + locations.Hba_file))
+	fNode.AddChild(tview.NewTreeNode("Ident_file: " + locations.Ident_file))
+	fNode.AddChild(tview.NewTreeNode("External_pid_file: " + locations.External_pid_file))
+
+	return fNode
+}
+
+func createSettingsNode(settings db.DbSettings) *tview.TreeNode {
+	sNode := tview.NewTreeNode("Settings")
+
 	sNode.AddChild(tview.NewTreeNode("SharedBufferSize: " + settings.SharedBufferSize))
 	sNode.AddChild(tview.NewTreeNode("MaintenanceWorkMem: " + settings.MaintenanceWorkMem))
 	sNode.AddChild(tview.NewTreeNode("TempBuffers: " + settings.TempBuffers))
@@ -40,13 +60,27 @@ func CreateInfoTree(db *db.DbConnection) *tview.TreeView {
 	sNode.AddChild(tview.NewTreeNode("AutovacuumEnabled: " + settings.AutovacuumEnabled))
 	sNode.AddChild(tview.NewTreeNode("Fillfactor: " + settings.Fillfactor))
 
-	fNode := tview.NewTreeNode("File Locations")
-	tree.GetRoot().AddChild(fNode)
-	fNode.AddChild(tview.NewTreeNode("Data_directory: " + locations.Data_directory))
-	fNode.AddChild(tview.NewTreeNode("Config_file: " + locations.Config_file))
-	fNode.AddChild(tview.NewTreeNode("Hba_file: " + locations.Hba_file))
-	fNode.AddChild(tview.NewTreeNode("Ident_file: " + locations.Ident_file))
-	fNode.AddChild(tview.NewTreeNode("External_pid_file: " + locations.External_pid_file))
+	return sNode
+}
 
-	return tree
+func createDbNode(connection db.DbCurrentConnection) *tview.TreeNode {
+	dbNode := tview.NewTreeNode("Database")
+	dbNode.SetSelectable(true)
+
+	dbNode.AddChild(tview.NewTreeNode("version: " + connection.Version))
+	dbNode.AddChild(tview.NewTreeNode("Size: " + connection.Size))
+
+	return dbNode
+}
+
+func createDbConnectionNode(connection db.DbCurrentConnection) *tview.TreeNode {
+	dbconnectionNode := tview.NewTreeNode("DatabaseConnection")
+	dbconnectionNode.SetSelectable(true)
+
+	dbconnectionNode.AddChild(tview.NewTreeNode("Host: " + "localhost"))
+	dbconnectionNode.AddChild(tview.NewTreeNode("Port: " + "5432"))
+	dbconnectionNode.AddChild(tview.NewTreeNode("User: " + connection.User))
+	dbconnectionNode.AddChild(tview.NewTreeNode("Database: " + connection.Database))
+
+	return dbconnectionNode
 }
